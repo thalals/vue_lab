04772ff66c6a612288ef0b8c29b0863f5751da1f
thalals <thalsal@naver.com>
2024-05-20 18:57:25 +0900

    #5 모달창 만들기, 열고 닫기 이벤트 함수생성

--- vuedongsan/src/App.vue
+++ vuedongsan/src/App.vue
@@ -4,9 +4,16 @@
     <a v-for="(menu, index) in menus" :key="index">{{ menu }}</a>
   </div>
 
+  <!-- 모달창 -->
+  <div class="black-bg" v-if="isModalOpen" v-on:click="modalClose()">
+    <div class="white-bg">
+      <p>dfdd</p>
+    </div>
+  </div>
+
   <main>
     <div class="products" v-for="(product, index) in products" :key="index">
-      <img src="./assets/logo.svg" class="room-img">
+      <img v-on:click="modalOpen();" src="./assets/logo.svg" class="room-img">
       <h3> {{ product }}</h3>
       <p> {{ prices[index] }} 만원 </p>
       <button v-on:click="increaseReports(index)">허위매물신고</button>
@@ -27,22 +34,34 @@ export default {
       css_color_style: 'color : blue',
       products: ['역삼동 원룸', '천호동 원룸', '마포구 원룸'],
       menus: ['Home', 'Shop', 'About'],
-      신고수: [0,0,0]
+      신고수: [0, 0, 0],
+      isModalOpen : false
     }
   },
 
-  methods : {
+  methods: {
     increaseReports(index) {
       this.신고수[index] += 1;
+    },
+    modalOpen() {
+      this.isModalOpen = true;
+    },
+    modalClose() {
+      this.isModalOpen = false;
     }
   }
 
 }
+
 </script>
 
 <style scoped>
-.menu {
+div{
+  box-sizing: border-box;
   text-align: center;
+}
+
+.menu {
   background: bisque;
   padding: 15px;
   border-radius: 10px;
@@ -52,11 +71,23 @@ export default {
   padding: 10px;
 }
 
-.products{
-  text-align: center;
-}
-.room-img{
+.room-img {
   width: 20%;
   margin-top: 40px;
 }
+
+.black-bg{
+  width: 50%;
+  background: #181818;
+  position: fixed;
+  padding: 20px;
+}
+
+.white-bg{
+  width: 100%;
+  background: white;
+  border-radius: 8px;
+  padding: 20px;
+}
+
 </style>
