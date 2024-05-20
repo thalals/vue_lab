18bdf9a9cd058cea56c59a6ee3ef2947b32b2d7c
thalals <thalsal@naver.com>
2024-05-20 17:43:27 +0900

    #1 데이터 바인딩

--- vuedongsan/src/App.vue
+++ vuedongsan/src/App.vue
@@ -1,21 +1,45 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
 </script>
 
 <template>
   <header>
     <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
   </header>
 
   <main>
-    <TheWelcome />
+    <div>
+      <!--  데이터 바인딩 1   -->
+      <h3 :style="css_color_style"> xx 원룸</h3>
+      <!--  데이터 바인딩 2   -->
+      <p> {{ price1 }} 만원 </p>
+    </div>
+    <div>
+      <h3> {{ products[0] }}</h3>
+      <p> {{ price2 }} 만원 </p>
+    </div>
+    <div>
+      <h3> {{ products[1] }}</h3>
+      <p> {{ price2 }} 만원 </p>
+    </div>
   </main>
 </template>
+
+<script>
+
+  //데이터 선언
+  export default {
+    name: 'App',
+    data(){
+      return {
+        price1 : 60,
+        price2 : 75,
+        css_color_style : 'color : blue',
+        products : ['역삼동 원룸', '천호동 원룸', '마포구 원룸']
+      }
+    }
+  }
+
+</script>
 
 <style scoped>
 header {
@@ -38,7 +62,7 @@ header {
     margin: 0 2rem 0 0;
   }
 
-  header .wrapper {
+  header {
     display: flex;
     place-items: flex-start;
     flex-wrap: wrap;
