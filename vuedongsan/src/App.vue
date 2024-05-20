8a71c8ca33ee77d12299b64b2a8261760fc63287
thalals <thalsal@naver.com>
2024-05-20 18:28:14 +0900

    #2 html 태그 v-for 문법

--- vuedongsan/src/App.vue
+++ vuedongsan/src/App.vue
@@ -2,24 +2,15 @@
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-  </header>
+
+  <div class="menu">
+    <a v-for="(menu, index) in menus" :key="index">{{ menu }}</a>
+  </div>
 
   <main>
-    <div>
-      <!--  데이터 바인딩 1   -->
-      <h3 :style="css_color_style"> xx 원룸</h3>
-      <!--  데이터 바인딩 2   -->
-      <p> {{ price1 }} 만원 </p>
-    </div>
-    <div>
-      <h3> {{ products[0] }}</h3>
-      <p> {{ price2 }} 만원 </p>
-    </div>
-    <div>
-      <h3> {{ products[1] }}</h3>
-      <p> {{ price2 }} 만원 </p>
+    <div v-for="(product, index) in products" :key="index">
+      <h3> {{ product }}</h3>
+      <p> {{ prices[index] }} 만원 </p>
     </div>
   </main>
 </template>
@@ -31,10 +22,10 @@
     name: 'App',
     data(){
       return {
-        price1 : 60,
-        price2 : 75,
+        prices : [60,70,80],
         css_color_style : 'color : blue',
-        products : ['역삼동 원룸', '천호동 원룸', '마포구 원룸']
+        products : ['역삼동 원룸', '천호동 원룸', '마포구 원룸'],
+        menus : ['Home', 'Shop', 'About']
       }
     }
   }
@@ -42,30 +33,15 @@
 </script>
 
 <style scoped>
-header {
-  line-height: 1.5;
+.menu {
+  text-align: center;
+  background: bisque;
+  padding: 15px;
+  border-radius: 10px;
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
+.menu a{
+  padding: 10px;
 }
 
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
 </style>
