469518de20a0b83d486d86ef227666183ed4d697
thalals <thalsal@naver.com>
2024-05-22 16:35:13 +0900

    #7 Component 문법 사용하기

--- vuedongsan/src/App.vue
+++ vuedongsan/src/App.vue
@@ -1,5 +1,12 @@
 <template>
 
+  <div class="menu">
+    <a v-for="(menu, index) in menus" :key="index">{{ menu }}</a>
+  </div>
+
+  <!-- Component  -->
+  <Discount></Discount>
+
   <!-- 모달창 -->
   <div class="black-bg" v-if="isModalOpen" v-on:click="modalClose()">
     <div class="white-bg">
@@ -8,45 +15,42 @@
     </div>
   </div>
 
-  <main>
-    <div class="menu">
-      <a v-for="(menu, index) in menus" :key="index">{{ menu }}</a>
-    </div>
+  <div class="products" v-for="(product, index) in 원룸들" :key="index">
+    <img v-on:click="modalOpen(product.image, product.content);" :src="product.image" class="room-img">
 
-    <div class="products" v-for="(product, index) in 원룸들" :key="index">
-      <img v-on:click="modalOpen(product.image, product.content);" :src="product.image" class="room-img">
+    <h3> {{ product.title }}</h3>
+    <p> {{ product.price }} 만원 </p>
+    <button v-on:click="increaseReports(index)">허위매물신고</button>
+    <br><span>신고수 : {{ 신고수[index] }}</span>
+  </div>
 
-      <h3> {{ product.title }}</h3>
-      <p> {{ product.price }} 만원 </p>
-      <button v-on:click="increaseReports(index)">허위매물신고</button>
-      <br><span>신고수 : {{ 신고수[index] }}</span>
-    </div>
-
-  </main>
 
 </template>
 
 <script>
 import data from './assets/data.js'
+import Discount from "@/components/Discount.vue";
 
 //데이터 선언
 export default {
   name: 'App',
+  components: {Discount},
+
   data() {
     return {
       prices: [60, 70, 80],
       css_color_style: 'color : blue',
       products: ['역삼동 원룸', '천호동 원룸', '마포구 원룸'],
       menus: ['Home', 'Shop', 'About'],
-      신고수: [0,0,0],
+      신고수: [0, 0, 0],
 
       // 모달 데이터
       modalImg: "",
       modalContent: "",
-      isModalOpen : false,
+      isModalOpen: false,
 
       // Import, export 데이터
-      원룸들 : data
+      원룸들: data
     }
   },
 
@@ -69,7 +73,7 @@ export default {
 </script>
 
 <style scoped>
-div{
+div {
   box-sizing: border-box;
   text-align: center;
 }
@@ -86,19 +90,26 @@ div{
   padding: 10px;
 }
 
+.discount{
+  background: lightgrey;
+  padding: 10px;
+  margin: 10px;
+  border-radius: 5px;
+}
+
 .room-img {
   width: 20%;
   margin-top: 40px;
 }
 
-.black-bg{
+.black-bg {
   width: 80%;
   background: #181818;
   position: fixed;
   padding: 20px;
 }
 
-.white-bg{
+.white-bg {
   width: 100%;
   background: white;
   border-radius: 8px;
---
+++ vuedongsan/src/components/Discount.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="discount">
+    <h4>지금 결제하면 20% 할인</h4>
+  </div>
+</template>
+
+<script>
+export  default {
+  // vue 파일의 name
+  name: 'Discount',
+}
+</script>
+
+<style>
+
+</style>
