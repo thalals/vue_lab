bf2ca5e6c81f6ab5b9bbcc44612413dff517e845
thalals <thalsal@naver.com>
2024-06-26 13:26:06 +0900

    #1 블로그 글목록 제작 (데이터바인딩, props)

--- blog/src/App.vue
+++ blog/src/App.vue
@@ -1,12 +1,36 @@
 <script>
+//component
+import List from './components/List.vue'
+//data
+import Articles from './assets/blog.js'
+
+export default {
+  name: 'App',
+  data() {
+    return {
+      articles: Articles,
+    }
+  },
+  components:{
+    List: List,
+  }
+}
 
 </script>
 
 <template>
-  <button type="button" class="btn btn-primary">Primary</button>
+  <div class="container mt-4">
+    <h5> 개발 블로그 입니다</h5>
+    <p> - Vue 로 만들었음 - </p>
+  </div>
+
+  <List :articles="articles"></List>
 
 </template>
 
 <style>
+#app{
+  text-align: center;
+}
 
 </style>
---
+++ blog/src/components/List.vue
@@ -0,0 +1,21 @@
+<script>
+export default {
+  name: "List",
+  props:{
+    articles: Array,
+  },
+
+}
+</script>
+
+<template>
+  <div v-for="(article, index) in articles" :key="index">
+    <h5> {{ article.title }} </h5>
+    <p> {{ article.content }} </p>
+    <p> {{ article.date }} </p>
+  </div>
+</template>
+
+<style>
+
+</style>
