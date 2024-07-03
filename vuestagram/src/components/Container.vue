3bf93d421d17bfc309adc64f094aaaf55cdde333
thalals <thalsal@naver.com>
2024-07-03 17:23:52 +0900

    #1 스타일 속성 데이터 바인딩

---
+++ vuestagram/src/components/Container.vue
@@ -0,0 +1,22 @@
+<script>
+import Post from "@/components/Post.vue";
+
+export default {
+  name: "Contatiner",
+  components: {Post},
+
+  props: {
+    posts: Array
+  }
+}
+</script>
+
+<template>
+  <div>
+    <Post v-for="(post, index) in posts" :key="index" :post="post" />
+  </div>
+</template>
+
+<style scoped>
+
+</style>
